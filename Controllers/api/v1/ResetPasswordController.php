969e275cf59dd1994971ddd353b492c145d2755c
Ashish Zarkar <azupworkuser@gmail.com>
2024-03-29 12:19:28 +0530

    Created forget and reset password functionality

---
+++ Controllers/api/v1/ResetPasswordController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\api\v1;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
+
+class ResetPasswordController extends Controller
+{
+    use ResetsPasswords;
+}
