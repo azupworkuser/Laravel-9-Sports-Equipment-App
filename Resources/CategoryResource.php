46b64d895a1fb5d6905fb5a6d2e6c568dd8da38b
Ashish Zarkar <azupworkuser@gmail.com>
2024-04-10 17:28:14 +0530

    Category module

---
+++ Resources/CategoryResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CategoryResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->getKey(),
+            'name' => $this->name,
+            'description' => $this->description,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'assets' => $this->whenLoaded('assets', function () {
+                return AssetResource::collection($this->assets);
+            }),
+            'products' => $this->whenLoaded('products', function () {
+                return ProductResource::collection($this->products);
+            }),
+        ];
+    }
+}
