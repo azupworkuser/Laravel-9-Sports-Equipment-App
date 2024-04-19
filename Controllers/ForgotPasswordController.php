695060a123f402d4c48ebbdce789852c119afa63
Ashish Zarkar <azupworkuser@gmail.com>
2024-04-19 17:28:17 +0530

    Confirm and forgot Passwords

---
+++ Controllers/ForgotPasswordController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+
+class ForgotPasswordController extends Controller
+{
+    use SendsPasswordResetEmails;
+}
