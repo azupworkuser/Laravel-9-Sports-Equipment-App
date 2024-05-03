0817959cb2d6993879bc85055cdf8f0c5a306547
Ashish Zarkar <azupworkuser@gmail.com>
2024-05-03 16:39:26 +0530

    Create user verification controller

---
+++ Controllers/VerificationController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\VerifiesEmails;
+use Illuminate\Http\Request;
+
+class VerificationController extends Controller
+{
+    use VerifiesEmails;
+
+    /**
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware('signed')->only('verify');
+        $this->middleware('throttle:6,1')->only('verify', 'resend');
+    }
+}
