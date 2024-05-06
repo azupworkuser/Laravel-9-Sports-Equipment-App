e0fdb0d203d21bcf98b29d78e1f3d968ca12d50a
Ashish Zarkar <azupworkuser@gmail.com>
2024-05-06 18:44:36 +0530

    Created listeners for send user invitation email

---
+++ Listeners/SendUserInvitationEmail.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\UserInvitation\UserInvitationCreated;
+use App\Mail\UserInvitationEmail;
+use App\Models\States\UserInvitation\Sent;
+use Illuminate\Mail\SentMessage;
+
+class SendUserInvitationEmail
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  UserInvitationCreated  $event
+     * @return void
+     */
+    public function handle(UserInvitationCreated $event)
+    {
+        $inviteEmailSent = \Mail::to($event->userInvitation->email)->send(
+            new UserInvitationEmail($event->userInvitation)
+        );
+
+        if ($inviteEmailSent instanceof SentMessage) {
+            $event->userInvitation->status->transitionTo(Sent::class);
+        }
+    }
+}
